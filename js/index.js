b70640d8dbd1303dbc3d09c994ec87ada8eaa112
anl2140 <59630586+anl2140@users.noreply.github.com>
2020-01-18 11:27:25 -0500

    complete slideshow

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen");
 
 var slideIndex = 1;
 showSlides(slideIndex);
